6c26dc8f28e0d92ff1f2bbb5c1e0b1fe0c718c44
gauravkr2506 <gauravkr2506@gmail.com>
2019-02-06 23:59:32 +0530

    finish

---
+++ src/messages/all-day.js
@@ -0,0 +1,249 @@
+export const All = () => {
+	return [
+		{
+			message: `💐💓💗💞💕💑💕💞💗💓💐
+It takes great luck to have 
+someone like you as a 
+boyfriend. I feel blessed 
+every day and every moment 
+for this gift. 
+I will love you till my last 
+breath no matter what life 
+brings before us!
+💐💓💗💞💕💑💕💞💗💓💐`,
+			message_type: "1",
+			language: "en"
+		},
+		{
+			message: `💐💓💗💞💕💑💕💞💗💓💐
+The more I spend time with 
+you, the more I fall in love 
+with you every day. You have 
+a very gentle and beautiful 
+heart that I promise to take 
+care of all my life. 
+I love you!
+💐💓💗💞💕💑💕💞💗💓💐`,
+			message_type: "1",
+			language: "en"
+		},
+		{
+			message: `💐💓💗💞💕💑💕💞💗💓💐
+My heart belongs to you 
+and it doesn’t want 
+anything but you. 
+You are the reason 
+for my smile and my 
+happiness. I love you!
+💐💓💗💞💕💑💕💞💗💓💐`,
+			message_type: "1",
+			language: "en"
+		},
+		{
+			message: `💐💓💗💞💕💑💕💞💗💓💐
+My world was so empty and dark 
+that it all seemed so 
+meaningless to me. 
+But when I met you, 
+suddenly it felt like the 
+sky over me has lightened 
+up by thousand stars. 
+I love you!
+💐💓💗💞💕💑💕💞💗💓💐`,
+			message_type: "1",
+			language: "en"
+		},
+		{
+			message: `💐💓💗💞💕💑💕💞💗💓💐
+It takes great luck to have 
+someone like you as a 
+boyfriend. I feel blessed 
+every day and every moment 
+for this gift. 
+I will love you till my last 
+breath no matter what life 
+brings before us!
+💐💓💗💞💕💑💕💞💗💓💐`,
+			message_type: "1",
+			language: "en"
+		},
+		{
+			message: `💐💓💗💞💕💑💕💞💗💓💐
+I was dreaming of an angel 
+to come in my life and shower 
+it with unlimited love. 
+Then I woke up and saw you. 
+I realized reality is more 
+beautiful than my dream was. 
+I am lucky to have you!
+💐💓💗💞💕💑💕💞💗💓💐`,
+			message_type: "1",
+			language: "en"
+		},
+		{
+			message: `💐💓💗💞💕💑💕💞💗💓💐
+Because you are in my life, 
+everything seems perfect and 
+nothing is out of balance. 
+I love you for making my 
+life a piece of heaven.
+💐💓💗💞💕💑💕💞💗💓💐`,
+			message_type: "1",
+			language: "en"
+		},
+		{
+			message: `💐💓💗💞💕💑💕💞💗💓💐
+You are my greatest comfort 
+and biggest inspiration. 
+You are the reason why my life 
+is so beautiful and my dreams 
+are so colorful.
+💐💓💗💞💕💑💕💞💗💓💐`,
+			message_type: "1",
+			language: "en"
+		},
+		{
+			message: `💐💓💗💞💕💑💕💞💗💓💐
+You are perfect even with all 
+of your imperfections. 
+You are amazing, even at 
+the times when you are 
+annoying. I love you 
+for everything!
+💐💓💗💞💕💑💕💞💗💓💐`,
+			message_type: "1",
+			language: "en"
+		},
+		{
+			message: `💐💓💗💞💕💑💕💞💗💓💐
+I find peace when I hide 
+myself in your arms. 
+I feel comfort when you 
+touch me. I want nothing more 
+than you and nothing less 
+than you!
+💐💓💗💞💕💑💕💞💗💓💐`,
+			message_type: "1",
+			language: "en"
+		},
+		{
+			message: `💐💓💗💞💕💑💕💞💗💓💐
+You can finish counting the 
+stars in the sky but you can 
+never finish counting the 
+love I have for you. Because 
+my heart is filled with 
+infinite love for you!
+💐💓💗💞💕💑💕💞💗💓💐`,
+			message_type: "1",
+			language: "en"
+		},
+		{
+			message: `💐💓💗💞💕💑💕💞💗💓💐
+My greatest fantasies are the 
+ones that start with you and 
+end with you. You are always 
+on my mind. I love you truly 
+madly and deeply!
+💐💓💗💞💕💑💕💞💗💓💐`,
+			message_type: "1",
+			language: "en"
+		},
+		{
+			message: `💐💓💗💞💕💑💕💞💗💓💐
+It takes great luck to have 
+someone like you as a 
+boyfriend. I feel blessed 
+every day and every moment 
+for this gift. 
+I will love you till my last 
+breath no matter what life 
+brings before us!
+💐💓💗💞💕💑💕💞💗💓💐`,
+			message_type: "1",
+			language: "en"
+		},
+		{
+			message: `💐💓💗💞💕💑💕💞💗💓💐
+You are the queen of my heart. 
+My heart finds pleasure in 
+following your commands at any 
+time and every time. 
+You are the one I love!
+💐💓💗💞💕💑💕💞💗💓💐`,
+			message_type: "1",
+			language: "en"
+		},
+		{
+			message: `💐💓💗💞💕💑💕💞💗💓💐
+I live every day hoping 
+to wake up someday with 
+you beside me. 
+Nothing can make me happier 
+than a day that starts with 
+you and ends with you!
+💐💓💗💞💕💑💕💞💗💓💐`,
+			message_type: "1",
+			language: "en"
+		},
+		{
+			message: `💐💓💗💞💕💑💕💞💗💓💐
+The Almighty has chosen you 
+to be my soulmate, as no one 
+else can understand me better 
+than you do. I love you, stay 
+with me always.
+💐💓💗💞💕💑💕💞💗💓💐`,
+			message_type: "1",
+			language: "en"
+		},
+		{
+			message: `💐💓💗💞💕💑💕💞💗💓💐
+It takes great luck to have 
+someone like you as a 
+boyfriend. I feel blessed 
+every day and every moment 
+for this gift. 
+I will love you till my last 
+breath no matter what life 
+brings before us!
+💐💓💗💞💕💑💕💞💗💓💐`,
+			message_type: "1",
+			language: "en"
+		},
+		{
+			message: `💐💓💗💞💕💑💕💞💗💓💐
+You have made me realized 
+that love is the best feeling 
+any human can feel. 
+Thank you for coming 
+in my life. I love you.
+💐💓💗💞💕💑💕💞💗💓💐`,
+			message_type: "1",
+			language: "en"
+		},
+		{
+			message: `💐💓💗💞💕💑💕💞💗💓💐
+I cannot imagine surviving 
+all the hardships of my life 
+without your love and support. 
+Keep holding my hands tightly 
+forever. I love you.
+💐💓💗💞💕💑💕💞💗💓💐`,
+			message_type: "1",
+			language: "en"
+		},
+		{
+			message: `💐💓💗💞💕💑💕💞💗💓💐
+I have been blessed by 
+the Almighty with such 
+a precious gem like you. 
+I will never let you go. 
+I love you.
+💐💓💗💞💕💑💕💞💗💓💐`,
+			message_type: "1",
+			language: "en"
+		},
+		
+	];
+};
